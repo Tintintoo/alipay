7a9da3b88f76523ae230ac993d7a6dbbdd4eecf9
wastong <waslcp@waslcpdeMacBook-Pro.local>
2016-07-14 15:30:42 +0800

    1

--- cloud.js
+++ cloud.js
@@ -50,7 +50,7 @@ AV.Cloud.define('clearQD', function(request, response) {
 });
 
 
-/*AV.Cloud.define('closeSoc', function(request, response) 
+AV.Cloud.define('closeSoc', function(request, response) 
 {
   // 知道 objectId，创建 AVObject
 	var post1 = AV.Object.createWithoutData('groupInfo','55e6f7c060b2fe714836657b');
@@ -89,7 +89,7 @@ post4.set('bshow', 0);
 post4.save();
 });
 
-AV.Cloud.define('openSo', function(request, response) 
+/*AV.Cloud.define('openSo', function(request, response) 
 {
 // 知道 objectId，创建 AVObject
 var post1 = AV.Object.createWithoutData('groupInfo', '55e6f7c060b2fe714836657b');
