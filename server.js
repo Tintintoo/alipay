8af175b319c41677526abc603f81f4d4a09717b7
wastong <waslcp@waslcpdeMacBook-Pro.local>
2016-07-14 11:56:12 +0800

    3

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 var AV = require('leanengine');
 
-var APP_ID = process.env.LEANCLOUD_APP_ID;
-var APP_KEY = process.env.LEANCLOUD_APP_KEY;
-var MASTER_KEY = process.env.LEANCLOUD_APP_MASTER_KEY;
+var APP_ID = process.env.LEANCLOUD_APP_ID|| 'ifmmzg0hwfo69zdho7dwwjlakqupnmjuukcab6pothxs50i0';
+var APP_KEY = process.env.LEANCLOUD_APP_KEY|| '0gdj48papdbw5qisfzq1wr4lmlzzpp6ebffiu6rn1voppfb6';
+var MASTER_KEY = process.env.LEANCLOUD_APP_MASTER_KEY|| 'j1vyg8hkoqfq2bul6n4ontu2yspylq7lk8nzjsi14ywbfo5r';
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 
