4ff57e75b2794b318a6add5a3f8bf2b888175ed2
wastong <waslcp@waslcpdeMacBook-Pro.local>
2016-07-14 16:09:00 +0800

    1

--- cloud.js
+++ cloud.js
@@ -129,16 +129,14 @@ post4.save();
 });
 
 
-/*AV.Cloud.define(‘destroyAnoymous’, function(request, response) 
-{
-console.log('start close anonymous ');
+AV.Cloud.define(‘destroyAnonymous', function(request, response) {
 // 知道 objectId，创建 AVObject
 var post1 = AV.Object.createWithoutData('groupInfo', '5677698460b2260ee44a6ae8');
 // 更改属性
-post1.set('bCanCreate', 0);
+post1.set('bCanCreate’,0);
 // 保存
 post1.save();
-});*/
+});
 
 AV.Cloud.define('openAnonymous', function(request, response) {
 // 知道 objectId，创建 AVObject
@@ -149,7 +147,7 @@ post1.set('bCanCreate',1);
 post1.save();
 });
 
-/*AV.Cloud.define('clearXZQD', function(request, response) {
+AV.Cloud.define('clearXZQD', function(request, response) {
 console.log('start clearXZQD');
 var query = new AV.Query('groupInfo');
 query.equalTo('Type', 1);
