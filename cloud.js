064baa47d58167c4d5204d1e0833c6e8a3ab951f
wastong <waslcp@waslcpdeMacBook-Pro.local>
2016-07-14 17:30:08 +0800

    1

--- cloud.js
+++ cloud.js
@@ -425,4 +425,29 @@ query.find({
 });
 });
 
+AV.Cloud.define('logRank', function(request, response) {
+var query = new AV.Query('chatUsers');
+query.limit(10);
+query.descending('dailylike');
+query.find({
+  success: function(results) {
+    // 处理返回的结果数据
+    for (var i = 0; i < results.length; i++) {
+      var object = results[i];
+      var post = new Post();
+      content = object.get('userID');
+      post.set('userID', content);
+      var content = object.get('dailylike');
+      post.set('dailylike', content);
+      post.set('rankType', '日魅力榜');
+    
+      post.save();
+    }
+  },
+  error: function(error) {
+    console.log('Error: ' + error.code + ' ' + error.message);
+  }
+});
+});
+
 module.exports = AV.Cloud;
