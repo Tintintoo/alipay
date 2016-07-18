01cf356332908e08ac30fd37062047d982cfa299
wastong <waslcp@waslcpdeMacBook-Pro.local>
2016-07-18 14:11:11 +0800

    wxpay1.1

--- cloud.js
+++ cloud.js
@@ -1,4 +1,5 @@
 var AV = require('leanengine');
+var WXPay = require("weixin");
 
 /**
  * 一个简单的云代码方法
@@ -19,20 +20,22 @@ response.success(date.getTime());
 //支付二次检测
 AV.Cloud.define('WxCreateUnifiedOrder', function(request, response)
 {
-  AV.Cloud.httpRequest({
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  url: 'http://www.weixin.qq.com/wxpay/pay.php',
-  body: request.params.receiptdata,
-  success: function(httpResponse) {
-    console.log('Request succ ' + httpResponse.text);
-    response.success(httpResponse.text);
-  },
-  error: function(httpResponse) {
-    console.error('Request failed with response code ' + httpResponse.status);
-  }
+var wxpay = WXPay({
+    appid: 'xxxxxxxx',
+    mch_id: '1234567890',
+    partner_key: 'xxxxxxxxxxxxxxxxx', //微信商户平台API密钥
+    pfx: fs.readFileSync('./wxpay_cert.p12'), //微信商户平台证书
+});
+wxpay.createUnifiedOrder({
+    body: '扫码支付测试',
+    out_trade_no: '20140703'+Math.random().toString().substr(2, 10),
+    total_fee: 1,
+    spbill_create_ip: '192.168.2.210',
+    notify_url: 'http://wxpay_notify_url',
+    trade_type: 'NATIVE',
+    product_id: '1234567890'
+}, function(err, result){
+    console.log(result);
 });
 });
 
